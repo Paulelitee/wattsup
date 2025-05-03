451cce011333e8fd0ff5e1ca37a09851e48fbda4
paulelite <paulelitehonour@gmail.com>
2025-05-03 15:02:53 -0700

    all files

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,16 @@
 function App() {
   return (
     <main>
-        <div className="py-8 px-4">
-           <h1>Estimate Your Electricity Bill</h1>
+        <div className="pt-16 px-4 bg-white">
+           <h1 className="">Estimate bill</h1>
+           <div className="flex flex-row justify--between w-full text-sm font-medium pt-4 ">
+            <div className="py-2 w-full text-sky-500 border-sky-500 border-b-1 flex flex-row justify-center">
+                Nepa
+            </div>
+            <div className="w-full flex flex-row justify-center py-2 text-gray-300">
+              Gen (coming soon)
+            </div>
+           </div>
        </div>
       </main>
   )
